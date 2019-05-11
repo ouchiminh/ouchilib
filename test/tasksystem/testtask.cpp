ac1880173f16d6c78f530ce355c175bd01d5c75b
ouchiminh <39828763+ouchiminh@users.noreply.github.com>
2019-05-11 17:29:48 +0900

    Update testtask.cpp

--- test/tasksystem/testtask.cpp
+++ test/tasksystem/testtask.cpp
@@ -14,7 +14,8 @@ DEFINE_TEST(test_task_instantiate)
     using namespace std::string_literals;
     task t1{ []() {return 0; }, "task1"s };
     task t2{ []() {return 1; }, "task2"s };
-    auto_arg_task t3(+[](int i, int j) {assert(i == 0 && j == 1); }, "task3"s, std::make_tuple("task1"s, "task2"s));
+    auto_arg_task t3(+[](int i, int j) {assert(i == 0 && j == 1); },
+                     "task3"s, std::make_tuple("task1"s, "task2"s));
     t1 --> t3;
     t2 --> t3;
     // because there is no task control system, 
