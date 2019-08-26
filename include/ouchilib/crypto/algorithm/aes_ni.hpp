5b7189b896cf27fdd79ad3bb2b9ac0e305c08cd0
ouchiminh <39828763+ouchiminh@users.noreply.github.com>
2019-08-27 03:17:17 +0900

    add aes_ni

---
+++ include/ouchilib/crypto/algorithm/aes_ni.hpp
@@ -0,0 +1,100 @@
+﻿#pragma once
+#include <wmmintrin.h>
+#include <cstdint>
+#include <cstdlib>
+#include "ouchilib/utl/step.hpp"
+#include "aes.hpp"
+#include "../common.hpp"
+
+namespace ouchi::crypto {
+
+template<size_t KeyLength>
+struct aes_ni {
+    static_assert(KeyLength == 16 || KeyLength == 24 || KeyLength == 32);
+private:
+    static constexpr std::uint_fast32_t nb = 4;
+    static constexpr std::uint_fast32_t nk = KeyLength / 4;
+    static constexpr std::uint_fast32_t nr = nk + 6;
+public:
+    static constexpr size_t block_size = nb * 4;
+    using block_t = memory_view<block_size>;
+    using key_t = memory_entity<KeyLength>;
+    using key_view = memory_view<KeyLength>;
+
+    aes_ni(key_view key)
+    {
+        set_key(key);
+    }
+    aes_ni() = default;
+    ~aes_ni()
+    {
+        using vp = std::decay_t<std::add_volatile_t<decltype(key_.data)>>;
+        using vpm = volatile __m128i*;
+        std::fill(const_cast<vp>(key_.data), const_cast<vp>(key_.data + KeyLength), 0);
+
+        for (auto [w,d] : ouchi::multiitr(w128, dw128)) {
+            ouchi::crypto::secure_memset((vpm)&w, 0);
+            ouchi::crypto::secure_memset((vpm)&d, 0);
+        }
+    }
+    void set_key(key_view key) noexcept
+    {
+        std::memcpy(key_.data, key.data, KeyLength);
+        expand_key();
+    }
+    void encrypt(block_t src, void* dest) const noexcept
+    {
+        __m128i state = _mm_loadu_si128(reinterpret_cast<const __m128i*>(src.data));
+        state = _mm_xor_si128(state, w128[0]);
+        for (auto r : ouchi::step(1u, nr)) {
+            state = _mm_aesenc_si128(state, w128[r]);
+        }
+        state = _mm_aesenclast_si128(state, w128[nr]);
+        _mm_storeu_si128(reinterpret_cast<__m128i*>(dest), state);
+    }
+    void decrypt(block_t src, void* dest) const noexcept
+    {
+        __m128i state = _mm_loadu_si128(reinterpret_cast<const __m128i*>(src.data));
+        state = _mm_xor_si128(state, dw128[nr]);
+        for (auto r = nr - 1; r > 0; --r) {
+            state = _mm_aesdec_si128(state, dw128[r]);
+        }
+        state = _mm_aesdeclast_si128(state, dw128[0]);
+        _mm_storeu_si128(reinterpret_cast<__m128i*>(dest), state);
+    }
+private:
+    alignas(16) __m128i w128[nr + 1];
+    alignas(16) __m128i dw128[nr + 1];
+    key_t key_;
+    void expand_key()
+    {
+        using aes = aes<KeyLength>;
+        constexpr int Rcon[10] = { 0x01,0x02,0x04,0x08,0x10,0x20,0x40,0x80,0x1b,0x36 };
+        std::uint32_t temp;
+        std::uint32_t w[60];
+
+        std::memcpy(w, key_.data, KeyLength);
+        for (size_t i = KeyLength / 4; i<nb*(nr + 1); i++) {
+            temp = w[i - 1];
+            if ((i % (KeyLength / 4)) == 0)
+                temp = aes::subword(rotword(temp)) ^ Rcon[i / (KeyLength / 4) - 1];
+            else if ((KeyLength / 4) > 6 && (i % (KeyLength / 4)) == 4)
+                temp = aes::subword(temp);
+            w[i] = w[i - (KeyLength / 4)] ^ temp;
+        }
+        for (auto r : ouchi::step(nr + 1)) {
+            w128[r] = _mm_loadu_si128(reinterpret_cast<__m128i*>(w + r * 4));
+        }
+        dw128[0] = w128[0];
+        for (auto i : ouchi::step(1u, nr)) {
+            dw128[i] = _mm_aesimc_si128(w128[i]);
+        }
+        dw128[nr] = w128[nr];
+    }
+
+};
+
+using aes128_ni = aes_ni<16>;
+using aes192_ni = aes_ni<24>;
+using aes256_ni = aes_ni<32>;
+}
--- test/crypto/test_aes.cpp
+++ test/crypto/test_aes.cpp
@@ -1,6 +1,7 @@
 ﻿#include <string>
 #include "../test.hpp"
 #include "ouchilib/crypto/algorithm/aes.hpp"
+#include "ouchilib/crypto/algorithm/aes_ni.hpp"
 #include "ouchilib/utl/multiitr.hpp"
 
 DEFINE_TEST(test_aes)
@@ -29,4 +30,22 @@ DEFINE_TEST(test_aes_memory)
 
     using namespace std::string_literals;
     CHECK_EQUAL("123456789abcdef"s, buf);
+}
+
+DEFINE_TEST(test_aes_ni)
+{
+    constexpr char buf[16] = "123456789abcdef";
+    char code[2][16];
+    char p[16];
+    char key[16] = "!!!!!!!!!!!!!!!";
+    ouchi::crypto::aes128_ni encoder(ouchi::crypto::memory_view<16>((void*)key));
+    ouchi::crypto::aes128 software_encoder(ouchi::crypto::memory_view<16>((void*)key));
+    encoder.encrypt(buf, code[0]);
+    software_encoder.encrypt(buf, code[1]);
+    encoder.decrypt(code[0], p);
+
+    using namespace std::string_literals;
+    CHECK_EQUAL("123456789abcdef"s, buf);
+    for (auto [a, b] : ouchi::multiitr{ code[0], code[1] })
+        CHECK_EQUAL(a, b);
 }
