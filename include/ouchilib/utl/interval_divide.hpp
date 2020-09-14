4422344b1d2b6cdc9bad9f8e1b22254847106d15
ouchiminh <39828763+ouchiminh@users.noreply.github.com>
2020-09-14 18:15:10 +0900

    update interval_divide

--- include/ouchilib/utl/interval_divide.hpp
+++ include/ouchilib/utl/interval_divide.hpp
@@ -3,21 +3,9 @@
 #include <concepts>
 #include <tuple>
 #include <utility>
+#include "step.hpp"
 
 namespace ouchi {
-
-template<class T>
-concept additive = requires(T t)
-{
-    t++;
-    ++t;
-    --t;
-    t--;
-    t + std::declval<int>();
-    std::declval<int>() + t;
-    t - std::declval<int>();
-    std::declval<int>() - t;
-};
 
 template<additive T>
 struct interval_divide {
@@ -25,39 +13,57 @@ struct interval_divide {
 
     interval_divide() = delete;
     template<class U = T, std::enable_if_t<std::is_same_v<T, U> && std::is_move_constructible_v<U>, int> = 0>
-    interval_divide(U&& begin, U&& end)
+    constexpr interval_divide(U&& begin, U&& end)
         : begin_{ std::forward<T>(begin) }
         , end_{ std::forward<T>(end) }
     {}
 
     template<class ...Arg1, class ...Arg2>
-    interval_divide(std::piecewise_construct_t, std::tuple<Arg1...> begin, std::tuple<Arg2...> end)
+    constexpr interval_divide(std::piecewise_construct_t, std::tuple<Arg1...> begin, std::tuple<Arg2...> end)
         : interval_divide(begin, end,
                           std::make_index_sequence<sizeof...(Arg1)>(),
                           std::make_index_sequence<sizeof...(Arg2)>())
     {}
 
     template<class U = T, std::enable_if_t<std::is_same_v<T, U> && std::is_copy_constructible_v<U>, int> = 0>
-    interval_divide(const interval_divide<U>& id)
+    constexpr interval_divide(const interval_divide<U>& id)
         : begin_{id.begin_}
         , end_{id.end_}
     {}
 
     template<class U = T, std::enable_if_t<std::is_same_v<T, U> && std::is_move_constructible_v<U>, int> = 0>
-    interval_divide(interval_divide<U>&& id)
+    constexpr interval_divide(interval_divide<U>&& id)
         : begin_{std::move(id.begin_)}
         , end_{std::move(id.end_)}
     {}
 
+    constexpr std::pair<additive_t, additive_t> divide(size_t current, size_t total)
+    {
+        size_t interval_count = end_ - begin_;
+        size_t r = interval_count % total;
+        size_t b = interval_count / total;
+        auto begin = begin_ + current * b + std::min(current, r);
+        return { begin, begin + b + (current < r ? 1 : 0) };
+    }
+    constexpr step<additive_t> divide_into_steps(size_t current, size_t total)
+    {
+        auto [begin, end] = divide(current, total);
+        return step(begin, end);
+    }
 private:
     template<class ...Arg1, class ...Arg2, size_t ...I1, size_t ...I2>
-    interval_divide(std::tuple<Arg1...>& t1, std::tuple<Arg2...>& t2, std::index_sequence<I1...>, std::index_sequence<I2...>)
+    constexpr interval_divide(std::tuple<Arg1...>& t1, std::tuple<Arg2...>& t2, std::index_sequence<I1...>, std::index_sequence<I2...>)
         : begin_(std::get<I1>(t1) ...)
         , end_(std::get<I2>(t2) ...)
     {}
 
     additive_t begin_, end_;
 };
+
+template<class T>
+interval_divide(T&&, T&&)->interval_divide<std::remove_reference_t<T>>;
+template<class T>
+interval_divide(const T&, const T&)->interval_divide<std::remove_reference_t<T>>;
 
 }
 
--- test/utl/test_interval_divide.cpp
+++ test/utl/test_interval_divide.cpp
@@ -1,5 +1,7 @@
 ﻿#include "../test.hpp"
 #include "ouchilib/utl/interval_divide.hpp"
+#include <vector>
+#include <numeric>
 
 using namespace ouchi;
 
@@ -9,5 +11,39 @@ DEFINE_TEST(test_interval_divide_instantiate)
     interval_divide<int>{id};
 
     interval_divide<int>{std::piecewise_construct, std::forward_as_tuple(0), std::forward_as_tuple(4)};
+}
+
+DEFINE_TEST(test_interval_divide_integral)
+{
+    interval_divide<int> id{ 1, 9 };
+    int sum = 0;
+    int count;
+    for (size_t i = 0; i < 3; ++i) {
+        count = 0;
+        for (auto s : id.divide_into_steps(i, 3)) {
+            sum += s;
+            ++count;
+        }
+        CHECK_TRUE(count <= 3);
+    }
+    CHECK_EQUAL(sum, 36);
+}
+
+DEFINE_TEST(test_interval_divide_container)
+{
+    std::vector<int> v(10);
+    std::iota(v.begin(), v.end(), 1);
+    interval_divide id{v.begin(), v.end()};
+    int sum = 0;
+    int count;
+    for (size_t i = 0; i < 3; ++i) {
+        count = 0;
+        for (auto s : id.divide_into_steps(i, 3)) {
+            sum += *s;
+            ++count;
+        }
+        CHECK_TRUE(count <= 4);
+    }
+    CHECK_EQUAL(sum, 55);
 }
 
